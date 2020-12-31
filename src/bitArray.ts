38634ed142790930275ff75876feb84c817a1db5
Jacob Daitzman <jdtzmn@gmail.com>
2020-12-30 22:21:42 -0500

    Remove circular dependencies

--- src/bitArray.ts
+++ src/bitArray.ts
@@ -1,6 +1,57 @@
-import { byteToBits, bitsToByte } from './byteConversion'
-
 export type Binary = 0 | 1
+
+// ------------------------------
+// - Helper Functions
+// ------------------------------
+
+/**
+ * @description Converts a byte to a bit array
+ * @export
+ * @param {number} byte The byte to convert to a bit array
+ * @returns The converted bit array
+ */
+export function byteToBits(byte: number) {
+  if (byte < 0 || byte > 255) throw new Error('byte value must be between 0 and 255')
+  if (!Number.isInteger(byte)) throw new Error('byte must be an integer')
+
+  const bitArray = new BitArray(8)
+  const bits = Array.from(byte.toString(2))
+
+  /**
+   * Start at the first defined bit since the `toString` method doesn't include padding 0's
+   *
+   * Example:
+   * 120.toString(2) is '1111000' which is not 8 bits long
+   */
+  for (let i = bitArray.length - bits.length; i < bitArray.length; i++) {
+    const bitValue = +bits.shift()! as Binary
+    bitArray.setAt(i, bitValue)
+  }
+
+  return bitArray
+}
+
+/**
+ * @description Convert a bit array to a byte number
+ * @export
+ * @param {BitArray} bits The bits to convert
+ * @returns The converted byte number
+ */
+export function bitsToByte(bits: BitArray) {
+  if (bits.length !== 8) throw new Error('bit array must be of length 8')
+
+  let byte = 0
+  for (let i = 0; i < bits.length; i++) {
+    const bit = bits.getAt(i)
+    byte += bit * Math.pow(2, bits.length - i - 1)
+  }
+
+  return byte
+}
+
+// ------------------------------
+// - BitArray Class
+// ------------------------------
 
 /**
  * @description A guard to ensure that a binary value is in fact binary
--- test/bitArray.test.ts
+++ test/bitArray.test.ts
@@ -1,5 +1,4 @@
-import BitArray from '../src/bitArray'
-import { byteToBits } from '../src/byteConversion'
+import BitArray, { byteToBits, bitsToByte } from '../src/bitArray'
 
 describe('BitArray class', () => {
   it('should be able to set bits in the bit array', () => {
@@ -114,5 +113,31 @@ describe('BitArray class', () => {
     const copy = bitArray.clone()
     copy.setAt(1, 0)
     expect(bitArray.getAt(1)).toBe(1)
+  })
+})
+
+describe('byte conversions', () => {
+  it('should be able to convert a byte to a bit array', () => {
+    const byte = 120
+    const bitArray = byteToBits(byte)
+    const idealBitArray = new BitArray([0, 1, 1, 1, 1, 0, 0, 0])
+    expect(bitArray.equals(idealBitArray)).toBeTruthy()
+  })
+
+  it('should throw errors if the byte is not a valid format', () => {
+    expect(() => {
+      byteToBits(-1)
+    }).toThrowError()
+  })
+
+  it('should throw an error if bitToByte bits are not of length 8', () => {
+    const bitArray = new BitArray(2)
+    expect(() => bitsToByte(bitArray)).toThrowError()
+  })
+
+  it('should convert a bit array to a byte', () => {
+    const bitArray = new BitArray([0, 1, 1, 1, 1, 0, 0, 0])
+    const byte = bitsToByte(bitArray)
+    expect(byte).toBe(120)
   })
 })
